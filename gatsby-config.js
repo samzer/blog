72f24042f8178e0231d5708ebca49389c7b83155
Samir Madhavan <samir.madhavan@gmail.com>
2024-12-08 12:33:28 +0530

    Refactor code

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,14 +73,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-  onCreateWebpackConfig: ({ actions }) => {
-    actions.setWebpackConfig({
-      resolve: {
-        fallback: {
-          "stream": require.resolve("stream-browserify")
-        }
-      }
-    });
-  }
+  ]
 }
